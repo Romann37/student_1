923d7137169a4ace8c540e28c5c465615e66e674
Romann37 <mihalkovo62@gmail.com>
2021-08-29 11:33:32 +0300

    задача 2

--- students.py
+++ students.py
@@ -3,15 +3,7 @@ class Mentor:
         self.name = name
         self.surname = surname
         self.courses_attached = []
-        
-    def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in student.grades:
-                student.grades[course] += [grade]
-            else:
-                student.grades[course] = [grade]
-        else:
-            return 'Ошибка'
+
 class Student:
     def __init__(self, name, surname, gender):
         self.name = name
@@ -23,11 +15,24 @@ class Student:
  
     def add_courses(self, course_name):
         self.finished_courses.append(course_name)
+    def rete_lec(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_attached and course in lecturer.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
 class Lecturer(Mentor):
            
     
 class Reviewer(Mentor):
-           
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'   
     
 
 
